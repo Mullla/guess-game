d77bd27956c1bb52bb4edb082b4794cdd171a51d
Mars <diablo_13_06@mail.ru>
2021-02-04 20:39:32 +0300

    added number of gueses

--- game/script.js
+++ game/script.js
@@ -5,46 +5,63 @@ let isNumber = function(n){
 }
 
 function guessNum(){
-    let num = 12; // загаданное число
+    let num = Math.floor(Math.random()*100) + 1, // загаданное число
+        gueses = 10; // количество попыток
 
-    // ввод числа от пользователя
-    let answer = prompt("Угадай число от 1 до 100");
+    function game(){
+        // ввод числа от пользователя
+        let answer = prompt("Угадай число от 1 до 100");
     
+        // если значение является числом, то сравниваем его
+        
+            if (isNumber(answer)){
+                gueses--;
+                console.log(gueses);
+                check();
+                
+                
+            } else if (typeof(answer) === 'string'){
+                alert("Введи число!");
+                game();
+            } else if (answer === null){
+                alert("Игра окончена");
+            }
+        
+        
 
-    // если значение является числом, то сравниваем его
-    if (isNumber(answer)){
-        check();
-    } else if (typeof(answer) === 'string'){
-        alert("Введи число!");
-        guessNum();
-    } else if (answer === null){
-        alert("Игра окончена");
-    }
-
-    // сравнивает введенное пользователем число и загаданное
-    function check(){
-        if (+answer === num){
-            alert("Поздравляю, Вы угадали!!!");
-        } else if (answer < num){
-            confirm("Загаданное число больше");
-            guessNum();
-        } else if (answer > num) {
-            confirm("Загаданное число меньше");
-            guessNum();
+        // сравнивает введенное пользователем число и загаданное
+        function check(){
+            if (+answer === num){
+                let decision = confirm(`Поздравляю, Вы угадали за ${gueses} попыток(-ки)!!! Хотели бы сыграть еще?`);
+            if (decision){
+                guessNum();
+                }
+            } else if (answer < num){
+                confirm(`Загаданное число больше, осталось попыток: ${gueses}`);
+                if (gueses > 0) {
+                    game();
+                } else {
+                    let decision = confirm("Попытки закончились, хотите сыграть еще?");
+                    if (decision){
+                        guessNum();
+                    }
+                }
+            } else if (answer > num) {
+                confirm(`Загаданное число меньше, осталось попыток: ${gueses}`);
+                if (gueses > 0) {
+                    game();
+                } else {
+                    let decision = confirm("Попытки закончились, хотите сыграть еще?");
+                    if (decision){
+                        guessNum();
+                    }
+                }
+            }
         }
+        
     }
+
+game();
 }
 
 guessNum();
-
-// let a = prompt('test');
-// console.log(a);
-// console.log(isNumber (a));
-
-// ok = '' / string
-// cancel = null / object
-
-// ok/cancel = 0 / number
-
-// ok = '' / false
-// cancel = null / false
